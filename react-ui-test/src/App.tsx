922b74e9fab558edb784a32ed2ebde6dd8c0e2ce
zaghax <zaghax@yahoo.es>
2022-09-10 13:51:02 -0500

    build: styled components library and linters added and configured

--- react-ui-test/src/App.tsx
+++ react-ui-test/src/App.tsx
@@ -9,12 +9,12 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    </p>
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
         >
           Learn React
         </a>
